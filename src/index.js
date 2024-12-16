2c4dc49c4205fba32ab1ad962df4486b80383b33
HGiang15 <gianghoang150503@gmail.com>
2024-12-16 18:59:21 +0700

    Get method and parameters

--- src/index.js
+++ src/index.js
@@ -20,15 +20,19 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
 app.get('/', (req, res) => {
   res.render('home');
 })                      
-
 app.get('/news', (req, res) => {
+  console.log(req.query.q);
   res.render('news');
-})        
-
+})  
+app.get('/blog', (req, res) => {
+  res.render('blog');
+})      
+app.get('/search', (req, res) => {
+  res.render('search');
+})   
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })      
