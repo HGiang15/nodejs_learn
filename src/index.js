3ae34819a8339d6f30d6a9081d5c1ec5d03b01c6
HGiang15 <gianghoang150503@gmail.com>
2024-12-17 20:17:17 +0700

    Create Controller and Router

--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3001
 
-// Khai bao path nghia la localhost:3001 la public
+const route = require('./routes')
+
+
 app.use(express.static(path.join(__dirname, 'public')))
 
-// HTTP logger
-app.use(morgan('combined'))
+app.use(express.urlencoded({
+  extended: true,
+}))
+app.use(express.json())
+
+app.use(morgan('combined')) // HTTP logger
 
 // Template engine
 // app.engine('handlebars', handlebars());
@@ -20,19 +26,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})                      
-app.get('/news', (req, res) => {
-  console.log(req.query.q);
-  res.render('news');
-})  
-app.get('/blog', (req, res) => {
-  res.render('blog');
-})      
-app.get('/search', (req, res) => {
-  res.render('search');
-})   
+route(app) // route init
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })      
